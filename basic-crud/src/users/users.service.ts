08742186d1602578ff0b20b434d2e422d09eb058
vasa <vasa@fedora.tail3d82ee.ts.net>
2024-06-12 21:17:24 +0530

    changed db manipulation

--- basic-crud/src/users/users.service.ts
+++ basic-crud/src/users/users.service.ts
@@ -2,63 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './users.entity'; // Correct import path
-import { send } from 'process';
 
 @Injectable()
 export class UsersService {
   constructor(
-    
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
 
   async findAll(): Promise<User[]> {
-    const query = `
-      SELECT * FROM users;
-    `;
-    return this.usersRepository.query(query);
+    return this.usersRepository.find();
   }
 
-  async findOne(id: number): Promise<User> {
-    const query = `
-      SELECT * FROM users
-      WHERE id = $1;
-    `;
-    const result = await this.usersRepository.query(query, [id]);
-    return result[0];
+  async findOne(id): Promise<User> {
+    return this.usersRepository.findOne(id);
   }
 
   async create(user: User): Promise<User> {
-    const query = `
-      INSERT INTO users (name, email, age) 
-      VALUES ($1, $2, $3) 
-      RETURNING *;
-    `;
-    const values = [user.name, user.email, user.age];
-    const result = await this.usersRepository.query(query, values);
-    return result[0];
+    const newUser = this.usersRepository.create(user);
+    return this.usersRepository.save(newUser);
   }
 
-  async update(id: number, user: User): Promise<User> {
-    const query = `
-      UPDATE users
-      SET name = $1, email = $2, age = $3
-      WHERE id = $4
-      RETURNING *;
-    `;
-    const values = [user.name, user.email, user.age, id];
-    const result = await this.usersRepository.query(query, values);
-    return result[0];
+  async update(id, user: User): Promise<User> {
+    await this.usersRepository.update(id, user);
+    return this.usersRepository.findOne(id);
   }
 
   async remove(id: number): Promise<void> {
-    const query = `
-      DELETE FROM users
-      WHERE id = $1;
-    `;
-    
-    await this.usersRepository.query(query, [id]);
-  
+    await this.usersRepository.delete(id);
   }
 }
-
