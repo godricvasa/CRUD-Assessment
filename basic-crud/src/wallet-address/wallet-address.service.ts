0fe07efd990f0d7d376e7e20a986895c2ce7fd35
vasa <vasa@fedora.tail3d82ee.ts.net>
2024-06-12 21:25:06 +0530

    changed queries and env variables

--- basic-crud/src/wallet-address/wallet-address.service.ts
+++ basic-crud/src/wallet-address/wallet-address.service.ts
@@ -12,51 +12,25 @@ export class WalletAddressService {
   ) {}
 
   findAll(): Promise<WalletAddress[]> {
-    const query = `
-      SELECT * FROM walletAddress;
-    `;
-    return this.walletAddressRepository.query(query)
-    // return this.walletAddressRepository.find();
+    return this.walletAddressRepository.find();
   }
 
   findOne(id): Promise<WalletAddress> {
-    const query = `
-    SELECT * FROM walletAddress
-    WHERE id = $1;
-  `;
-    return this.walletAddressRepository.query(query,[id]);
+    return this.walletAddressRepository.findOne(id);
   }
 
   async create(walletAddress: WalletAddress): Promise<WalletAddress> {
-    const createdAt = new Date();
-    const query = `
-      INSERT INTO walletAddress (userid, address, createdat) 
-      VALUES ($1, $2, $3) 
-      RETURNING *;
-    `;
-    const values = [ walletAddress.user,walletAddress.address, createdAt];
-    const result = await this.walletAddressRepository.query(query, values);
-    return result[0];
+    walletAddress.createdAt = new Date();
+    const newWalletAddress = this.walletAddressRepository.create(walletAddress);
+    return this.walletAddressRepository.save(newWalletAddress);
   }
-  
 
-  async update(id: number, walletAddress: WalletAddress): Promise<WalletAddress> {
-    const query = `
-      UPDATE walletAddress
-      SET address = $1, userId = $2
-      WHERE id = $3
-      RETURNING *;
-    `;
-    const values = [walletAddress.address, walletAddress.user, id];
-    const result = await this.walletAddressRepository.query(query, values);
-    return result[0];
+  async update(id, walletAddress: WalletAddress): Promise<WalletAddress> {
+    await this.walletAddressRepository.update(id, walletAddress);
+    return this.walletAddressRepository.findOne(id);
   }
 
   async remove(id: number): Promise<void> {
-    const query = `
-      DELETE FROM walletAddress
-      WHERE id = $1;
-    `;
-    await this.walletAddressRepository.query(query, [id]);
+    await this.walletAddressRepository.delete(id);
   }
 }
